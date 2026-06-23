85aab7180aff4b9f6de4c396acef0c5bcd77fc98
Chinmay Patel <chinmaypatel1180@gmail.com>
2026-06-23 16:57:39 +0530

    UDF-1

---
+++ UDF-No argument, No Return Type.py
@@ -0,0 +1,17 @@
+# No argument, No Return Type
+
+# Example 1 (Call Function Multiple Time using Loop)
+
+def show():
+     print("Hello World") 
+for i in range(5):
+    show()
+
+
+# Example 2
+    def sum(): 
+        n1 = 10 
+        n2 = 20 
+        ans = n1+n2 
+        print("Sum is ", ans) 
+    sum()
