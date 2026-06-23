9825966e3379dc2fac444ee41905028bbfb646ac
Chinmay Patel <chinmaypatel1180@gmail.com>
2026-06-23 17:07:04 +0530

    UDF-2
    python

---
+++ UDF-Argument No Return.py
@@ -0,0 +1,17 @@
+# Argument No Return
+
+
+# program 1
+def sum(n1, n2): 
+    ans = n1+n2 
+    print("Sum is ", ans) 
+sum(10, 20)
+
+# program 2
+def evenodd(no):
+    if no % 2 == 0:
+        print("No is Even")
+    else:
+        print("No is Odd")
+evenodd(2)
+evenodd(3)
